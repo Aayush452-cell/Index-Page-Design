722c79830991bd79f4650c13aa6886db86ceb478
Aayush Kumar Singh <malemeaayushkumarsingh@gmail.com>
2021-04-04 18:44:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return "Hello World"
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
+
